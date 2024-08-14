0061b76d8cffe143afe782f92344513bcfe51f9d
yogeshwera96 <168747660+yogeshwera96@users.noreply.github.com>
2024-08-14 12:08:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,7 @@ app.use('/api/user', userRoutes)
 
 //deployment
 
-const _dirname1 = path.resolve()
-if(process.env.NODE_ENV === "production"){
-app.use(express.static(path.join(_dirname1,"/frontend/build")))
-app.get("*", (req, res) => {
-res.sendFile(path.resolve(_dirname1, "frontend", "build", "index.html"))
-})
+
 
 }
 //deployment
